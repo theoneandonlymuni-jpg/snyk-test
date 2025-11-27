0de014b478f1da12eed82498be55a505daecfe26
theoneandonlymuni-jpg <theoneandonlymuni@gmail.com>
2025-11-28 01:54:16 +0300

    Update sample_code.py

--- sample_code.py
+++ sample_code.py
@@ -1,1 +1,11 @@
+import os
+import subprocess
+
+user_input = input("Enter a command to run: ")
+os.system(user_input)
+
+USERNAME = "admin"
+PASSWORD = "12345"
+
+subprocess.call("ls -la", shell=True)
 
